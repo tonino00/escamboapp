8628c233f93b7bea83e3d96440e25add8f2f3509
tonino00 <antoninocgama@hotmail.com>
2018-03-30 14:08:43 -0300

    ajust in devise registration for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
-  namespace :backoffice do
-  get 'categories/index'
-  end
-
-
+  
 
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resources:categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
+    get 'admins/index'
   end
 
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations]
   devise_for :members
 
   root 'site/home#index'
